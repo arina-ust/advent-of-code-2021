1684cbffcf85b2617d12e941afe6f7cd322d2f68
arina-ust <arina.ustyantseva@storytel.com>
2021-12-04 09:23:59 +0100

    Day 4 (same for part 2)

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -37,11 +37,47 @@ def day_4_1(path):
     return sum_uncalled * winner
 
 
+def day_4_2(path):
+    dimension = 5
+    numbers, boards, boards_visited = read_boards(path, dimension)
+    latest_winner = -1
+    latest_board_winner_index = -1
+    won_boards = set()
+    for num in numbers:
+        for board_index in range(len(boards)):
+            if board_index in won_boards:
+                continue
+            for row_index in range(dimension):
+                for n in range(dimension):
+                    if num == boards[board_index][row_index][n]:
+                        boards_visited[board_index][row_index][n] = 1
+                        if sum(boards_visited[board_index][row_index]) == dimension or \
+                                find_column_sum(boards_visited[board_index], dimension) == dimension:
+                            latest_winner = num
+                            latest_board_winner_index = board_index
+                            won_boards.add(latest_board_winner_index)
+    print(numbers)
+    print(boards)
+    print(boards_visited)
+    print("Last board to win #", latest_board_winner_index)
+
+    board_winner, board_winner_visited = boards[latest_board_winner_index], boards_visited[latest_board_winner_index]
+    sum_uncalled = 0
+    for i in range(dimension):
+        for j in range(dimension):
+            if board_winner_visited[i][j] == 0:
+                sum_uncalled += board_winner[i][j]
+    print("Sum uncalled = ", sum_uncalled)
+    return sum_uncalled * latest_winner
+
+
 def find_column_sum(board, dimension):
     column_sum = 0
     for n in range(dimension):
         for row_index in range(dimension):
             column_sum += board[row_index][n]
+        if column_sum == dimension:
+            return column_sum
         column_sum = 0
     return column_sum
 
@@ -70,5 +106,3 @@ def read_boards(path, dimension):
             board.append(row)
             board_visited.append(row_visited)
     return numbers, boards, boards_visited
-
-
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from day_01.day_01 import day_1_2, day_1_1
 from day_02.day_02 import day_2_1, day_2_2
 from day_03.day_03 import day_3_1, day_3_2
-from day_04.day_04 import day_4_1
+from day_04.day_04 import day_4_1, day_4_2
 
 if __name__ == '__main__':
     # print(day_1_1("day_01/full.txt"))  # expected 7 for easy
@@ -11,5 +11,6 @@ if __name__ == '__main__':
     # print(day_2_2("day_02/full.txt"))  # expected 900 for easy
     # print(day_3_1("day_03/full.txt"))  # expected 198 for easy
     # print(day_3_2("day_03/full.txt"))  # expected 230 for easy
-    print(day_4_1("day_04/full.txt"))  # expected 4512 for easy
+    # print(day_4_1("day_04/full.txt"))  # expected 4512 for easy
+    print(day_4_2("day_04/full.txt"))  # expected 4512 for easy
 
