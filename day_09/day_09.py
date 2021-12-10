2d50a59fccc4d59f28e8e9fc29fed239ea0e45cb
arina-ust <arina.ustyantseva@storytel.com>
2021-12-10 17:45:24 +0100

    Day 9 Part 2

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -2,16 +2,17 @@ import common
 
 
 def day_9_1(path):
-    local_mins = find_local_mins(path)
+    grid = common.read_int_list_of_lists(path)
+    local_mins = find_local_mins(grid)[0]
 
     print(local_mins)
 
     return sum([x + 1 for x in local_mins])
 
 
-def find_local_mins(path):
-    grid = common.read_int_list_of_lists(path)
+def find_local_mins(grid):
     local_mins = []
+    coords = []
     for y in range(len(grid)):
         for x in range(len(grid[0])):
             e = grid[y][x]
@@ -32,8 +33,51 @@ def find_local_mins(path):
             m = min(neighbors)
             if e < m:
                 local_mins.append(e)
-    return local_mins
+                coords.append((x, y))
+    return local_mins, coords
 
 
 def day_9_2(path):
-    local_mins = find_local_mins(path)
+    grid = common.read_int_list_of_lists(path)
+    local_mins_coords = find_local_mins(grid)[1]
+
+    basins_size = []
+    for c in local_mins_coords:
+        basin = []
+        find_basin(grid, c, basin, {})
+        s = len(basin)
+
+        if len(basins_size) < 3:
+            basins_size.append(s)
+            continue
+
+        search = [b for b in basins_size]
+        search.append(s)
+        m = min(search)
+        if m == s:
+            continue
+        basins_size.remove(m)
+        basins_size.append(s)
+
+    return basins_size[0] * basins_size[1] * basins_size[2]
+
+
+def find_basin(grid, start_coord, basin_values, seen_coords: dict):
+    x = start_coord[0]
+    y = start_coord[1]
+
+    if x < 0 or y < 0 or x >= len(grid[0]) or y >= len(grid) or start_coord in seen_coords:
+        return
+
+    seen_coords[start_coord] = True
+
+    e = grid[y][x]
+    if e == 9:
+        return
+
+    basin_values.append(e)
+
+    find_basin(grid, (x-1, y), basin_values, seen_coords)
+    find_basin(grid, (x+1, y), basin_values, seen_coords)
+    find_basin(grid, (x, y-1), basin_values, seen_coords)
+    find_basin(grid, (x, y+1), basin_values, seen_coords)
