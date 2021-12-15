6ece9f34434356fbaba83df361d69ed28be413ea
arina-ust <arina.ustyantseva@storytel.com>
2021-12-15 18:05:59 +0100

    Day 14 part 2

--- day_14/day_14.py
+++ day_14/day_14.py
@@ -48,8 +48,59 @@ def get_counts(template):
     return letter_counts
 
 
-def increase_count(letter, letter_counts):
+def increase_count(letter, letter_counts, by=1):
     if letter in letter_counts:
-        letter_counts[letter] += 1
+        letter_counts[letter] += by
     else:
-        letter_counts[letter] = 1
+        letter_counts[letter] = by
+
+
+def day_14_2(path):
+    lines = common.read_string_list(path)
+    template = lines[0]
+    insertion_rules = get_pairs_dict(lines[2:])
+    pair_counts = get_pair_counts(template)
+    letter_counts = get_counts(template)
+    n_steps = 40
+
+    next_pair_counts = pair_counts.copy()
+    for step in range(n_steps):
+        for pair, count in pair_counts.items():
+            if next_pair_counts[pair] == 0:
+                continue
+            new_pairs = insertion_rules[pair]
+            for p in new_pairs:
+                if p in next_pair_counts:
+                    next_pair_counts[p] += count
+                else:
+                    next_pair_counts[p] = count
+            if next_pair_counts[pair] > 0:
+                next_pair_counts[pair] -= count
+            increase_count(new_pairs[0][1], letter_counts, count)
+        pair_counts = next_pair_counts.copy()
+
+    min_counts = sys.maxsize
+    max_counts = 0
+    for count in letter_counts.values():
+        min_counts = min(min_counts, count)
+        max_counts = max(max_counts, count)
+
+    return max_counts - min_counts
+
+
+def get_pairs_dict(lines):
+    rules = {}
+    for line in lines:
+        parts = [p.strip() for p in line.split(" -> ")]
+        rules[parts[0]] = [parts[0][0] + parts[1], parts[1] + parts[0][1]]
+    return rules
+
+
+def get_pair_counts(template):
+    pairs = {}
+    for i in range(len(template) - 1):
+        if template[i:i+2] in pairs:
+            pairs[template[i:i+2]] += 1
+        else:
+            pairs[template[i:i+2]] = 1
+    return pairs
